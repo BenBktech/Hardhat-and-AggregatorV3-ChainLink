0f44165ba639ead1eee1dd2e7b66ad79b0faecfe
BenBktech <bbrucher@gmail.com>
2022-10-07 15:17:58 +0200

    More tests

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,10 +1,12 @@
 const { deployments, ethers, getNamedAccounts } = require("hardhat");
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 
 describe("FundMe", async function() {
     let fundMe
     let deployer
     let mockV3Aggregator
+    const sendValue = ethers.utils.parseEther("1")
+
     beforeEach(async function() {
         //deploy our fundMe contract using hardhat-deploy
         //deploy everything in one line
@@ -20,6 +22,24 @@ describe("FundMe", async function() {
         it("sets the aggregator addresses correctly", async function() {
             const response = await fundMe.priceFeed()
             assert.equal(response, mockV3Aggregator.address)
+        })
+    })
+
+    describe("fund", async function() {
+        it("Fails if you don't send enough ETH", async function() {
+            await expect(fundMe.fund()).to.be.reverted;
+        })
+        it("update the amount funded data structure", async function() {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.addressToAmountFunded(
+                deployer
+            )
+            assert.equal(response.toString(), sendValue.toString())
+        })
+        it("Adds funder to array of funders", async function() {
+            await fundMe.fund({ value: sendValue })
+            const funder = await fundMe.funders(0);
+            assert.equal(funder, deployer)
         })
     })
 })
